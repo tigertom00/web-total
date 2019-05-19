2e96ee55d129df9127260b7574999e1c69a16568
tigertom00 <tigertom00@gmail.com>
2019-05-19 21:25:20 +0200

    email

--- srv/settings/base.py
+++ srv/settings/base.py
@@ -112,6 +112,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend'
 )
+DEFAULT_FROM_EMAIL = 'Admin <postmaster@nxfs.no>'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
