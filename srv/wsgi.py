020913a4a5adc6e2b9c737dd87cb8970f9ae554a
tigertom00 <tigertom00@gmail.com>
2019-05-19 16:25:25 +0200

    wsgi

--- srv/wsgi.py
+++ srv/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'srv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'srv.settings.prod')
 
 application = get_wsgi_application()
